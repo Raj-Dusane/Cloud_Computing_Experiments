c5179995e3fdbb7f33141e26c8f2167861508abe
raj-dusane <dusaneraj15@gmail.com>
2024-10-18 13:46:46 +0530

    update completed

--- exp6/my-react-project/src/App.js
+++ exp6/my-react-project/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h3>Experiment no: 6</h3>
-        <h4>Raj Dusane | 42415</h4>
+        <h4>Akansha | 42466</h4>
 
         <img src={logo} className="App-logo" alt="logo" />
 
